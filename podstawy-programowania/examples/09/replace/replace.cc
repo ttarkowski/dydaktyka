53f12b8bf60cf7647dc222ed4dd9904aae399c38
Tomasz Tarkowski <Tomasz.Tarkowski.TT@gmail.com>
2023-12-05 19:14:17 +0100

    Przykład użycia referencji.

---
+++ podstawy-programowania/examples/09/replace/replace.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+void
+replace(std::string& s, char from, char to)
+{
+  for (std::size_t i = 0; i < s.size(); ++i) {
+    if (s.at(i) == from) {
+      s.at(i) = to;
+    }
+  }
+}
+
+int
+main()
+{
+  std::string s = "Programowanie w C++";
+  std::cout << "Napis oryginalny:    " << s << '\n';
+  replace(s, ' ', '*');
+  std::cout << "Napis zmodyfikowany: " << s << '\n';
+}
