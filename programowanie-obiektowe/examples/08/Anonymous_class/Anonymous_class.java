a775865eaa48c99e1dd03e608c24f6b3a483c1d3
Tomasz Tarkowski <Tomasz.Tarkowski.TT@gmail.com>
2024-05-14 02:12:48 +0200

    Przykład.

---
+++ programowanie-obiektowe/examples/08/Anonymous_class/Anonymous_class.java
@@ -0,0 +1,25 @@
+class My_class {
+    public void f() {
+        System.out.println("My_class.f");
+    }
+}
+
+interface My_interface {
+    void f();
+}
+
+public class Anonymous_class {
+    public static void main(String args[]) {
+        new My_class() {
+            @Override public void f() {
+                System.out.println("<anonymous class/class>.f");
+            }
+        }.f();
+
+        new My_interface() {
+            @Override public void f() {
+                System.out.println("<anonymous class/interface>.f");
+            }
+        }.f();        
+    }
+}
